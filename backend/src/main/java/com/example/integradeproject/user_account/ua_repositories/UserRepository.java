2d7ec6f7fa5b34aca9036ead25f43496813e2286
natdnai tessapan <natdanai.tess@gmail.com>
2024-09-24 16:13:31 +0700

    pbi22 refresh token

--- backend/src/main/java/com/example/integradeproject/user_account/ua_repositories/UserRepository.java
+++ backend/src/main/java/com/example/integradeproject/user_account/ua_repositories/UserRepository.java
@@ -3,10 +3,8 @@ package com.example.integradeproject.user_account.ua_repositories;
 import com.example.integradeproject.user_account.ua_entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
 
-    Optional<User> findByOid(String ownerId);
+    User findByOid(String ownerId);
 }
