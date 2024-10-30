9c4310680b60ff4b67ced38f8bcf61358bd7a54c
ศิระกาญ กาจนวิลาสกุล <sirakran.kran@mail.kmutt.ac.th>
2024-10-31 02:13:16 +0700

    tooltip and disable the visibility button for non owner

--- frontend/src/component/ToggleVisible.vue
+++ frontend/src/component/ToggleVisible.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <button
-      class="toggle-button fixed bottom-24 right-8 w-14 h-14 rounded-full flex items-center justify-center text-white text-2xl cursor-pointer"
-      @click="showModal"
+      class="toggle-button fixed bottom-24 right-8 w-14 h-14 rounded-full flex items-center justify-center text-white text-2xl"
+      :disabled="!isOwner"
+      @click="isOwner ? showModal() : null"
+      v-tooltip="
+        !isOwner ? 'You need to be board owner to perform this action.' : ''
+      "
     >
       <i :class="isPublic ? 'fa fa-globe' : 'fa fa-lock'"></i>
     </button>
@@ -23,21 +27,35 @@ import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import VisibleModal from './VisibleModal.vue'
 import fetchUtils from '../lib/fetchUtils'
+import { checkOwnership } from '../lib/utils.js'
 
+// Define all refs at the start
 const route = useRoute()
 const boardId = route.params.boardId
 const isPublic = ref(false)
 const isPrivate = ref(false)
 const isModalVisible = ref(false)
 const modalMessage = ref('')
+const isOwner = ref(false)
 
-// Fetch board data
 const fetchBoardData = async () => {
   try {
     const boardData = await fetchUtils.getBoards(boardId)
     isPublic.value = boardData.data.visibility === 'public'
     isPrivate.value = boardData.data.visibility === 'private'
-    console.log('isPublic:', isPublic.value, 'isPrivate:', isPrivate.value)
+
+    // Get current user from localStorage
+    const currentUser = localStorage.getItem('username')?.trim()
+    isOwner.value = checkOwnership(boardData, currentUser)
+
+    console.log(
+      'isPublic:',
+      isPublic.value,
+      'isPrivate:',
+      isPrivate.value,
+      'isOwner:',
+      isOwner.value
+    )
   } catch (error) {
     console.error('Error fetching board data:', error)
   }
@@ -46,15 +64,17 @@ const fetchBoardData = async () => {
 // Show modal
 const showModal = () => {
   if (isPublic.value) {
-    modalMessage.value = 'private?'
+    modalMessage.value = 'Make board private?'
   } else if (isPrivate.value) {
-    modalMessage.value = '"public?'
+    modalMessage.value = 'Make board public?'
   }
 
   isModalVisible.value = true
 }
 
-onMounted(fetchBoardData)
+onMounted(() => {
+  fetchBoardData()
+})
 </script>
 
 <style scoped>
@@ -66,5 +86,10 @@ onMounted(fetchBoardData)
 
 .toggle-button:hover {
   transform: scale(1.1) rotate(360deg);
+}
+
+.toggle-button:disabled {
+  cursor: not-allowed;
+  opacity: 0.6;
 }
 </style>
